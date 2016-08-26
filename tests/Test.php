3670d5ee4e70258302f6104f351e90542f5d453f
Billy Renyut <engineer@haryono.co.id>
2016-08-26 16:26:30 +0700

    add more case into testing

--- tests/Test.php
+++ tests/Test.php
@@ -12,5 +12,39 @@ class Test extends PHPUnit_Framework_TestCase
 
 		$this->assertEquals('lari',$hasil);
 
+		$kata = 'memukul';
+		$hasil = $stemmer->process($kata);
+
+		$this->assertEquals('pukul',$hasil);
+
+		$kata = 'melakukan';
+		$hasil = $stemmer->process($kata);
+		
+		$this->assertEquals('laku',$hasil);
+
+		$kata = 'melindungi';
+		$hasil = $stemmer->process($kata);
+		
+		$this->assertEquals('lindung',$hasil);
+
+		$kata = 'pengalaman';
+		$hasil = $stemmer->process($kata);
+		
+		$this->assertEquals('alam',$hasil);
+
+		$kata = 'penyerangan';
+		$hasil = $stemmer->process($kata);
+		
+		$this->assertEquals('serang',$hasil);
+
+		$kata = 'pemutihan';
+		$hasil = $stemmer->process($kata);
+		
+		$this->assertEquals('putih',$hasil);
+
+		$kata = 'tersebar';
+		$hasil = $stemmer->process($kata);
+		
+		$this->assertEquals('sebar',$hasil);
 	}
 }
